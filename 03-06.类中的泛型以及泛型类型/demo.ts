9cf0f6d89a2ce20b388ed93049965fea798bf22c
LAPTOP-EA4HC9EB\15110 <15110320048@163.com>
2023-03-29 22:49:19 +0800

    wip: T

--- 03-06.类中的泛型以及泛型类型/demo.ts
+++ 03-06.类中的泛型以及泛型类型/demo.ts
@@ -1,32 +1,47 @@
+// *********泛型继承接口类型 start************
 // interface Item {
 //   name: string;
 // }
 
-// class DataManager<T extends number | string> {
+// class DataManager2<T extends Item> {
 //   constructor(private data: T[]) {}
-//   getItem(index: number): T {
-//     return this.data[index];
+//   getItem(index: number): string {
+//     return this.data[index].name;
 //   }
 // }
 
-// const data = new DataManager<number>([1]);
-// data.getItem(0);
-
-// const data = new DataManager([
+// const data = new DataManager2([
 //   {
 //     name: 'dell'
 //   }
 // ]);
+// *********泛型继承接口类型 end************
 
-// interface Test {
-//   name: string;
-// }
+// *********泛型限制某一类型 start************
 
-// const data = new DataManager<number>([]);
+class DataManager<T extends number | string> {
+  constructor(private data: T[]) {}
+  getItem(index: number): T {
+    return this.data[index];
+  }
+}
+// const data = new DataManager<number>([1]);
+// data.getItem(0);
+
+// *********泛型限制某一类型 end************
+
+const data = new DataManager<number>([]);
 
 // 如何使用泛型作为一个具体的类型注解
 function hello<T>(params: T) {
   return params;
 }
 
+//  <T>(param: T):返回值类型为泛型T
+//  = hello 具体实现的函数
 const func: <T>(param: T) => T = hello;
+
+// () => string 函数返回值类型。后边为实现方式
+const func2: () => string = () => {
+  return "String";
+};
